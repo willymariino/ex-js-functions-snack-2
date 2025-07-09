e8f0d5f8497dd4c7ae77c0743e3c766403787833
willymariino <wg.mariino@gmail.com>
2025-07-09 14:46:47 +0200

    first commit

---
+++ snack_funzioni.js
@@ -0,0 +1,9 @@
+/*
+🏆 Snack 1
+
+- Crea una funzione che somma due numeri.
+- Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+- Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+- Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+*/
+
